9eac3959fd01dfe6410434f505bfd111bc63352c
Salvatore <workstudioms@gmail.com>
2025-07-08 22:19:27 +0200

    feat: Milestone 4, complete.

--- src/main.ts
+++ src/main.ts
@@ -99,3 +99,51 @@ async function getActress(id: number): Promise<Actress | null> {
   }
 }
 
+// Crea una funzione getAllActresses che chiama:
+
+// GET / actresses
+
+// La funzione deve restituire un array di oggetti Actress.
+
+// Può essere anche un array vuoto.
+
+async function getAllActresses(): Promise<Actress[]> {
+  try {
+    const response = await fetch('http://localhost:3333/actresses');
+
+    if (!response.ok) {
+      throw new Error(`Errore HTTP ${response.status}: ${response.statusText}`);
+    }
+
+    const dati: unknown = await response.json();
+
+    if (!(dati instanceof Array)) {
+      throw new Error("Formato dei dati non valido: non è un array!");
+    }
+
+    const attriciValide: Actress[] = dati.filter(isActress);
+
+    return attriciValide;
+
+  } catch (errore) {
+
+    if (errore instanceof Error) {
+      console.error('Errore durante il recupero dei dati:', errore.message)
+    } else {
+      console.error('Errore sconosciuto:', errore)
+    }
+
+    return []
+
+  }
+}
+
+// Milestone 5
+
+// Crea una funzione getActresses che riceve un array di numeri(gli id delle attrici).
+
+// Per ogni id nell’array, usa la funzione getActress che hai creato nella Milestone 3 per recuperare l’attrice corrispondente.
+
+//   L'obiettivo è ottenere una lista di risultati in parallelo, quindi dovrai usare Promise.all.
+
+// La funzione deve restituire un array contenente elementi di tipo Actress oppure null(se l’attrice non è stata trovata).
